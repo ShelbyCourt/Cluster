436ca04478a27be05963495bd7bbeb8cdeb82727
ShelbyCourt <shelby.b.courtney@gmail.com>
2020-07-06 15:41:25 -0700

    Reads and downloads from S3 Bucket

--- src/Components/Downloads/Downloads.js
+++ src/Components/Downloads/Downloads.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import './DownloadsSass.css';
+import "./DownloadsSass.css";
 
 class Downloads extends Component {
   render() {
@@ -9,6 +9,32 @@ class Downloads extends Component {
         <div className="containerDL">
           <h1>Learn On the Go!</h1>
           <p>Take a few notes with you:</p>
+          <ul>
+            <li>
+              <a
+                href="https://clusteringly-downloads.s3-us-west-1.amazonaws.com/Beginner-Classroom-Phrases.PNG"
+                target="_blank"
+              >
+                Beginner Classroom/Virtual Event Phrases
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://clusteringly-downloads.s3-us-west-1.amazonaws.com/Beginner-Introduction-Phrases.png"
+                target="_blank"
+              >
+                Beginner Self-Introduction Phrases
+              </a>{" "}
+            </li>
+            <li>
+              <a
+                href="https://clusteringly-downloads.s3-us-west-1.amazonaws.com/Places-to-go1.png"
+                target="_blank"
+              >
+                Places to go
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     );
